04ed67c78becb27ec769f58c853bc3d2d88cafa3
Jesse Page <jesse.page@dominos.com>
2018-02-14 10:59:03 -0500

    Updating Biz and dLive Liferay themes

---
+++ builds/ant-builds/live-theme_2018-02-13/docroot/js/src/element-selectors.js
@@ -0,0 +1,15 @@
+// ELEMENT SELECTORS
+
+//j(document).ready(function () {
+//  j(".primary_nav_wrap").sticky({
+//    topSpacing: 0,
+//    zIndex: 302,
+//    getWidthFrom: 'body',
+//    responsiveWidth: true
+//  });
+//  
+//  j(".site-navigation").sticky({
+//    topSpacing: 50,
+//    zIndex: 302
+//  });
+//});
